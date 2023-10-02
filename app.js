fd32ed8e5b3484ff4b10d3b7170ea9887572e58c
unknown <ranatanveerhassan04@gmail.com>
2023-10-02 22:53:54 +0500

    ok

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("Welcome my web page")
+var num = prompt("Enter Your Fist Name ");
+var num1 = prompt(" Enter Your last Name");
+var num2 = prompt("Enter Your Age");
+var num3 = prompt("Enter City Name")
+var num4 = prompt("Enter Phone Number")
+var good = "Name:" + num + num1 + "\n" + "Age:" + num2 + "\n" + 
+"City:" + num3 + "\n" + "Phone#:" + num4 + "\n" + "Thank you";
+alert(good);
+
+var yourAge= prompt("what is your Name")
+if(yourAge >= 18){
+    alert("correct")
+}else{
+    alert("Not correct")
+}
+
+var use= prompt("what is your Name")
+if(use == "tanveer"){
+    alert("correct")
+}else{
+    alert("Not correct")
+}
