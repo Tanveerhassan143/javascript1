ce26db03b764f59167a9921f064c79cdb36890ab
unknown <ranatanveerhassan04@gmail.com>
2023-10-03 14:38:44 +0500

    is ok

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 alert("Welcome my web page")
-var num = prompt("Enter Your Fist Name ");
-var num1 = prompt(" Enter Your last Name");
+var num = prompt("Enter Your Fist Name");
+var num1 = prompt("Enter Your last Name");
 var num2 = prompt("Enter Your Age");
 var num3 = prompt("Enter City Name")
 var num4 = prompt("Enter Phone Number")
-var good = "Name:" + num + num1 + "\n" + "Age:" + num2 + "\n" + 
+var good = "Name:" + num +  num1 + "\n" + "Age:" + num2 + "\n" + 
 "City:" + num3 + "\n" + "Phone#:" + num4 + "\n" + "Thank you";
 alert(good);
 
-var yourAge= prompt("what is your Name")
+var yourAge= prompt("what is your age")
 if(yourAge >= 18){
     alert("correct")
 }else{
